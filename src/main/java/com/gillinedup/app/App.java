9baaacd7ee2b5bff10d9307f5618e8e441fe98ca
GillinedUp <myrreah@gmail.com>
2017-11-03 17:18:56 +0100

    added solver from Jama

--- src/main/java/com/gillinedup/app/App.java
+++ src/main/java/com/gillinedup/app/App.java
@@ -3,45 +3,23 @@ package com.gillinedup.app;
 import java.util.List;
 import java.util.Map;
 
+import Jama.Matrix;
 import com.gillinedup.graph.*;
 
 public class App {
 	public static void main(String[] args) {
-//		Graph g = new Graph();
-//
-//		g.addBidirectionalEdge(1,2);
-//		g.addBidirectionalEdge(1,3);
-//		g.addBidirectionalEdge(3,4);
-//		g.addBidirectionalEdge(3,5);
-//		g.addBidirectionalEdge(4,5);
-//		g.addBidirectionalEdge(4,6);
-//		g.addBidirectionalEdge(5,6);
-//		g.addBidirectionalEdge(6,2);
-//
-//		CycleUtil cycleUtil = new CycleUtil(g);
-//		List<Graph> cycles = cycleUtil.listAllCycles();
-//        CycleTransform cycleTransform = new CycleTransform(cycles);
-//        List<List<Edge>> rawCycles = cycleTransform.getCyclesFromGraphs();
-//        List<List<Edge>> processedCycles = cycleTransform.removeRedundantEdges(rawCycles);
-//        cycleTransform.sortCyclesByLength(processedCycles);
-//        cycleTransform.removeRedundantCycles(processedCycles);
-//
-//        for(List<Edge> cycle : processedCycles) {
-//            for (Edge e : cycle) {
-//                System.out.print(e + " ");
-//            }
-//            System.out.println();
-//        }
 		Graph g = new Graph();
 
-        g.addBidirectionalVoltageEdge(1, 2, 24.0);
-        g.addBidirectionalEdge(1,2, 4.0);
-        g.addBidirectionalVoltageEdge(1,4, 28.0);
-        g.addBidirectionalEdge(2,3, 1.0);
-        g.addBidirectionalEdge(2,5, 2.0);
-        g.addBidirectionalVoltageEdge(3,6, 7.0);
-        g.addBidirectionalEdge(4,5, 0.0);
-        g.addBidirectionalEdge(5,6, 0.0);
+        g.addBidirectionalEdge(1, 2, 7.0);
+        g.addBidirectionalVoltageEdge(7, 1, 300.0);
+        g.addBidirectionalEdge(2, 3, 2.0);
+        g.addBidirectionalEdge(2, 4, 5.0);
+        g.addBidirectionalEdge(3, 5, 4.0);
+        g.addBidirectionalEdge(3, 6, 6.0);
+        g.addBidirectionalEdge(4, 5, 1.0);
+        g.addBidirectionalEdge(4, 6, 11.0);
+        g.addBidirectionalEdge(5, 6, 9.0);
+        g.addBidirectionalEdge(6, 7, 0.0);
 
 		CycleUtil cycleUtil = new CycleUtil(g);
 		List<Graph> cycles = cycleUtil.listAllCycles();
@@ -50,11 +28,37 @@ public class App {
 		List<List<Edge>> processedCycles = cycleTransform.removeRedundantEdges(rawCycles);
 		cycleTransform.sortCyclesByLength(processedCycles);
 		cycleTransform.removeRedundantCycles(processedCycles);
-        for(List<Edge> cycle : processedCycles) {
-            for (Edge e : cycle) {
-                System.out.print(e + " ");
-            }
-            System.out.println();
+//        for(List<Edge> cycle : processedCycles) {
+//            for (Edge e : cycle) {
+//                System.out.print(e + " ");
+//            }
+//            System.out.println();
+//        }
+        MatrixPreparer matrixPreparer = new MatrixPreparer(processedCycles);
+        matrixPreparer.fillMap();
+        matrixPreparer.fillMatrix();
+//        double[][] matrix = matrixPreparer.getMatrix();
+//        double[] vector = matrixPreparer.getVector();
+//        for (double[] aMatrix : matrix) {
+//            for (double anAMatrix : aMatrix) {
+//                System.out.print(anAMatrix + " ");
+//            }
+//            System.out.println();
+//        }
+//        for (double aVector : vector) {
+//            System.out.println(aVector);
+//        }
+        double[] vector1 = matrixPreparer.getVector();
+        int n = vector1.length;
+        double[][] vector = new double[n][1];
+        for (int i = 0; i < n; i++) {
+            vector[i][0] = vector1[i];
+        }
+        Matrix a = new Matrix(matrixPreparer.getMatrix());
+        Matrix b = new Matrix(vector);
+        Matrix x = a.solve(b);
+        for (int i = 0; i < n; i++) {
+            System.out.println(x.get(i, 0));
         }
 
 	}
