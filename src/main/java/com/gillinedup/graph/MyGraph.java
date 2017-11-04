e244b40947c955a23d3aa45bc82dbafd3d6fc6b8
GillinedUp <myrreah@gmail.com>
2017-11-04 13:32:54 +0100

    refactoring

--- src/main/java/com/gillinedup/graph/MyGraph.java
+++ src/main/java/com/gillinedup/graph/MyGraph.java
@@ -8,73 +8,31 @@ import java.util.Set;
 public class MyGraph {
 	private final List<Vertex> vertices;
 	private final List<Edge> edges;
-
-	/**
-	 * Constructor for <code>MyGraph</code>.
-	 * 
-	 */
+    
 	public MyGraph() {
 		vertices = new ArrayList<>();
 		edges = new ArrayList<>();
 	}
 
-	/**
-	 * Constructor for <code>MyGraph</code>.
-	 * 
-	 * @param vertices
-	 *            <code>List</code> of <code>Vertex</code>s in the
-	 *            <code>MyGraph</code>.
-	 * @param edges
-	 *            <code>List</code> of <code>Edge</code>s in the
-	 *            <code>MyGraph</code>.
-	 */
-	public MyGraph(List<Vertex> vertices, List<Edge> edges) {
+	MyGraph(List<Vertex> vertices, List<Edge> edges) {
 		this.vertices = vertices;
 		this.edges = edges;
 	}
 
-	/**
-	 * Return the <code>List</code> of <code>Vertex</code>s in the
-	 * <code>MyGraph</code>.
-	 * 
-	 * @return <code>List</code> of <code>Vertex</code>s in the
-	 *         <code>MyGraph</code> .
-	 */
 	public List<Vertex> getVertices() {
 		return vertices;
 	}
 
-	/**
-	 * Return the <code>List</code> of <code>Edge</code>s in the
-	 * <code>MyGraph</code>.
-	 * 
-	 * @return <code>List</code> of <code>Edge</code>s in the <code>MyGraph</code>
-	 *         .
-	 */
-	public List<Edge> getEdges() {
+	List<Edge> getEdges() {
 		return edges;
 	}
 
-	/**
-	 * Removes and <code>Edge</code> from the graph.
-	 * 
-	 * @param edge
-	 */
-	public void removeEdge(Edge edge) {
+	void removeEdge(Edge edge) {
 		edges.remove(edge);
 	}
 
-	/**
-	 * Returns <code>Set<Vertex></code> of neighbor <code>Vertex</code> for
-	 * <code>node</code> .
-	 * 
-	 * @param node
-	 *            <code>Set<Vertex></code> of adjacent <code>Vertex</code> for
-	 *            <code>node</code> .
-	 * @return
-	 */
-	public Set<Vertex> getNeighbors(Vertex node) {
-		Set<Vertex> neighbors = new HashSet<Vertex>();
+	Set<Vertex> getNeighbors(Vertex node) {
+		Set<Vertex> neighbors = new HashSet<>();
 		for (Edge edge : edges) {
 			if (edge.getSource().equals(node)) {
 				neighbors.add(edge.getDestination());
@@ -82,32 +40,6 @@ public class MyGraph {
 		}
 		return neighbors;
 	}
-
-	/**
-	 * Add forward and backwards (directed) edges to the <code>MyGraph</code>
-	 * connecting two <code>Vertex</code> that hold data <code>src</code> and
-	 * <code>dest</code>.
-	 * 
-	 * @param src
-	 *            <code>Point</code> data for source <code>Vertex</code>
-	 * @param dest
-	 *            <code>Point</code> data for destination <code>Vertex</code>
-	 */
-//	public void addBidirectionalEdge(Point src, Point dest) {
-//		Vertex sourceVertex = new Vertex("Vertex (" + src.getX() + "," + src.getY() + ")", src);
-//		Vertex destinationVertex = new Vertex("Vertex (" + dest.getX() + "," + dest.getY() + ")", dest);
-//
-//		if (!vertices.contains(sourceVertex)) {
-//			vertices.add(sourceVertex);
-//		}
-//
-//		if (!vertices.contains(destinationVertex)) {
-//			vertices.add(destinationVertex);
-//		}
-//
-//		edges.add(new Edge(sourceVertex, destinationVertex));
-//		edges.add(new Edge(destinationVertex, sourceVertex));
-//	}
 
 	void addBidirectionalEdge(int id1, int id2) {
         addBidirectionalEdge(id1, id2, 0.0);
@@ -144,6 +76,7 @@ public class MyGraph {
         Edge edge1 = new Edge(sourceVertex, destinationVertex, w);
         edge1.setVoltage();
         edge1.setVoltageDirection(1);
+
         Edge edge2 = new Edge(destinationVertex, sourceVertex, w);
         edge2.setVoltage();
         edge2.setVoltageDirection(-1);
